3cfa8a31377bcdccb5ab10a50ba7d4a5af1899c2
mduniphin <maxx.duniphin@gmail.com>
2017-04-01 03:40:28 -0500

    adjusted port to work with Heroku

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // load modules
-var http = require('http');
+const http = require('http');
 
 // set port number
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
-function handleRequest(req, resp){
-		resp.end('<h1>Welcome to my webpage</h1>');
+function handleRequest(res,res){
+  res.end('<h1>Welcome to my webpage</h1>');
 }
 
-var server = http.createServer(handleRequest);
+const server = http.createServer(handleRequest);
 
-//start our server
-server.listen(PORT, function(){
-	//this is a callback, get used to it now
-	console.log("Server is running on port: %s", PORT);
+// start the server
+server.listen(PORT, ()=>{
+  console.log(`Magic happens on port ${PORT}`);
 });
